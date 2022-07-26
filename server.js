774d8dad51fe59cbe30aa44f994fbbd6f3baa90b
Quentin Bourgeois <quentintgc@live.fr>
2022-07-26 17:24:07 +0200

    first commit

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 const express = require("express");
 const cors = require("cors");
-//permettre l’accés au backend
 const app = express();
 var corsOptions = {
 origin: "http://localhost:8080"
 };
 app.use(cors(corsOptions));
-// changer les requests de content-type - application/json
 app.use(express.json());
-// changer les requests content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
 const db = require("./app/models");
@@ -30,7 +27,7 @@ process.exit();
 app.get("/", (req, res) => {
 res.json({ message: "welcome to REST DEMO." });
 });
-require("./app/routes/tutorial.routes")(app);
+require("./app/routes/users.routes")(app);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
